6577d497febf38a5604a8fa69d7941fef2a039fd
Premnath Ramanathan <prem_chess@yahoo.co.in>
2020-02-17 01:11:02 -0600

    Added Profile screen related code. Added Tabbar

---
+++ Health4TheWorldCourses/utilities/ProfileStruct.swift
@@ -0,0 +1,14 @@
+//
+//  ProfileStruct.swift
+//  Health4TheWorldCourses
+//
+//  Created by Premnath Ramanathan on 2/16/20.
+//  Copyright © 2020 Health4TheWorld. All rights reserved.
+//
+
+import Foundation
+
+struct ProfileStruct {
+    let ProfileItems: [String]
+    let segueLinks: [String]
+}
