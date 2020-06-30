f559c7034a4c2bbb16f3ec224831df558360d538
Premnath Ramanathan <prem_chess@yahoo.co.in>
2020-06-29 19:14:13 -0500

    Separate storyboards for login and home. Login flows added

--- Health4TheWorldCourses/Login Screens/LoginHomeVC.swift
+++ Health4TheWorldCourses/Login Screens/LoginHomeVC.swift
@@ -11,7 +11,16 @@ import UIKit
 class LoginHomeVC: UIViewController {
     @IBOutlet weak var label1: UILabel!
     @IBOutlet weak var label2: UILabel!
+    @IBOutlet weak var stackView: UIStackView!
     
+    @IBOutlet weak var googleSignIn: UIButton!
+    @IBOutlet weak var fbSignIn: UIButton!
+    @IBOutlet weak var emailSignIn: UIButton!
+    
+    @IBOutlet weak var stackView2: UIStackView!
+    @IBOutlet weak var alreadyUserLabel: UILabel!
+    @IBOutlet weak var LogInButton: UIButton!
+    @IBOutlet weak var SkipButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,8 +32,33 @@ class LoginHomeVC: UIViewController {
         label1.text = ConstantTexts.loginHomePage.label1Text
         label2.text = ConstantTexts.loginHomePage.label2Text
         
+        setupStackView1()
         
     }
-
+    
+    func setupStackView1(){
+        googleSignIn.setTitle(Constants.ButtonTitles.googleButtonTitle, for: .normal)
+        fbSignIn.setTitle(Constants.ButtonTitles.facebookButtonTitle, for: .normal)
+        emailSignIn.setTitle(Constants.ButtonTitles.emailSignUpTitle, for: .normal)
+        
+        
+        
+    }
+    
+    @IBAction func googleSignInClicked(_ sender: Any) {
+    }
+    
+    @IBAction func fbSignInClicked(_ sender: Any) {
+    }
+    
+    @IBAction func emailSignInClicked(_ sender: Any) {
+    }
+    
+    @IBAction func LoginClicked(_ sender: Any) {
+    }
+    
+    @IBAction func skipClicked(_ sender: Any) {
+    }
+    
 }
 
